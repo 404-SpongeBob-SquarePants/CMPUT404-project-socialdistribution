d94614c3d7f732663b127c4f550f330e267a7df8
Yuan Wang <wang17@ualberta.ca>
2020-02-16 13:46:09 -0700

    update models

--- mysite/wePost/models.py
+++ mysite/wePost/models.py
@@ -1,55 +1,40 @@
 from django.db import models
 import uuid
 
-CONTENT_TYPE_CHOICES = (
-    ('text/markdown', 'text/markdown'),
-    ('text/plain', 'text/plain'),
-    ('application/base64', 'application/base64'),
-    ('image/png;base64', 'image/png;base64'),
-    ('image/jpeg;base64', 'image/jpeg;base64'),
-)
 # Create your models here.
 class Author(models.Model):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
     email = models.EmailField(unique = True)
-    bio = models.TextField()
     host = models.URLField()
-    firstName = models.CharField(max_length=256)
-    lastName = models.CharField(max_length=256)
-    displayName = models.CharField(max_length=256)
+    displayName = models.CharField(max_length = 256)
     url = models.URLField()
     github = models.URLField()
 
 class Post(models.Model):
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=256)
-    source = models.URLField()
-    origin = models.URLField()
-    description = models.CharField(max_length=512)
-    contentType = models.CharField(max_length=32, choices=CONTENT_TYPE_CHOICES)
-    content = models.TextField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    count = models.PositiveIntegerField()
-    size = models.PositiveIntegerField()
-    published = models.DateTimeField(auto_now_add=True)
-
-    VISIBILITY_CHOICES =(
+    VISIBILITYCHOICES =(
         ('PUBLIC', 'PUBLIC'),
         ('FOAF', 'FOAF'),
         ('FRIENDS', 'FRIENDS'),
         ('PRIVATE', 'PRIVATE'),
         ('SERVERONLY', 'SERVERONLY'),
     )
-    visibility = models.CharField(max_length=16, choices=VISIBILITY_CHOICES)
-    unlisted = models.BooleanField(default=False)
+
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    title = models.CharField(max_length = 256)
+    content = models.TextField()
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)
+    count = models.PositiveIntegerField()
+    published = models.DateTimeField(auto_now_add = True)
+    visibility = models.CharField(max_length = 16, choices = VISIBILITYCHOICES, default = 'PUBLIC')
+    visibleTo = models.TextField(null = True, blank = True)
+    unlisted = models.BooleanField(default = False)
 
 class Comment(models.Model): 
  
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)    
-    podstId = models.ForeignKey(Post, on_delete=models.CASCADE)        
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)                                        
-    comment = models.CharField(max_length=400)
-    contentType = models.CharField(max_length=32, choices=CONTENT_TYPE_CHOICES)
-    published = models.DateTimeField(auto_now_add=True, blank=True)
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)    
+    post = models.ForeignKey(Post, on_delete = models.CASCADE)        
+    author = models.ForeignKey(Author, on_delete = models.CASCADE)                                        
+    comment = models.CharField(max_length = 400)
+    published = models.DateTimeField(auto_now_add = True, blank = True)
