7fa0277e5ab8f880abcd46aba3f79ec4a94bee0d
Isaac <Isaaczhang11@gmail.com>
2020-04-02 19:37:15 -0600

    restrict mardown content done

--- src/Register.js
+++ src/Register.js
@@ -2,7 +2,7 @@ import React from "react";
 // import ReactDOM from "react-dom";
 import "antd/dist/antd.css";
 import './components/Register.css';
-import { Form, Input, Button} from 'antd';
+import { Form, Input, Button, Layout, Card} from 'antd';
 import axios from 'axios';
 import _ from "lodash"
 import { BE_REGISTER_API_URL, FE_LOGIN_URL } from "./utils/constants.js";
@@ -73,101 +73,92 @@ l
     const { getFieldDecorator } = this.props.form;
 
     const formItemLayout = {
+      labelAlign : "left",
       labelCol: {
         xs: { span: 24 },
-        sm: { span: 8 }
+        sm: { span: 10 }
       },
       wrapperCol: {
         xs: { span: 24 },
-        sm: { span: 8 }
-      }
-    };
-    const tailFormItemLayout = {
-      wrapperCol: {
-        xs: {
-          span: 24,
-          offset: 0
-        },
-        sm: {
-          span: 16,
-          offset: 8
-        }
+        sm: { span: 14 }
       }
     };
 
     return (
-      <div className='register'>
-        <Form {...formItemLayout}>
+      <Layout>
+        <Layout.Content>
+          <Card className="register-card">
+            <Form {...formItemLayout}>
+              <Form.Item
+                label={
+                  <span>
+                    Username&nbsp;
+                </span>
+                }
+              >
+                {getFieldDecorator("username", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please input your username!",
+                      whitespace: true
+                    }
+                  ]
+                })(<Input />)}
+              </Form.Item>
 
-          <Form.Item
-            label={
-              <span>
-                Username&nbsp;
-            </span>
-            }
-          >
-            {getFieldDecorator("username", {
-              rules: [
-                {
-                  required: true,
-                  message: "Please input your username!",
-                  whitespace: true
-                }
-              ]
-            })(<Input />)}
-          </Form.Item>
+              <Form.Item label="E-mail">
+                {getFieldDecorator("email", {
+                  rules: [
+                    {
+                      type: "email",
+                      message: "The input is not valid E-mail!"
+                    },
+                    {
+                      required: true,
+                      message: "Please input your E-mail!"
+                    }
+                  ]
+                })(<Input />)}
+              </Form.Item>
 
-          <Form.Item label="E-mail">
-            {getFieldDecorator("email", {
-              rules: [
-                {
-                  type: "email",
-                  message: "The input is not valid E-mail!"
-                },
-                {
-                  required: true,
-                  message: "Please input your E-mail!"
-                }
-              ]
-            })(<Input />)}
-          </Form.Item>
+              <Form.Item label="Password" hasFeedback>
+                {getFieldDecorator("password", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please input your password!"
+                    },
+                    {
+                      validator: this.validateToNextPassword
+                    }
+                  ]
+                })(<Input.Password />)}
+              </Form.Item>
+              <Form.Item label="Confirm Password" hasFeedback>
+                {getFieldDecorator("confirm", {
+                  rules: [
+                    {
+                      required: true,
+                      message: "Please confirm your password!"
+                    },
+                    {
+                      validator: this.compareToFirstPassword
+                    }
+                  ]
+                })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+              </Form.Item>
+              <Form.Item wrapperCol={ {span : 24} }>
+                <Button type="primary" htmlType="button" onClick={this.handleSubmit}>
+                  Register
+                </Button>
+                <a className="back-to-login" href={FE_LOGIN_URL}> Back to log in </a>
+              </Form.Item>
 
-          <Form.Item label="Password" hasFeedback>
-            {getFieldDecorator("password", {
-              rules: [
-                {
-                  required: true,
-                  message: "Please input your password!"
-                },
-                {
-                  validator: this.validateToNextPassword
-                }
-              ]
-            })(<Input.Password />)}
-          </Form.Item>
-          <Form.Item label="Confirm Password" hasFeedback>
-            {getFieldDecorator("confirm", {
-              rules: [
-                {
-                  required: true,
-                  message: "Please confirm your password!"
-                },
-                {
-                  validator: this.compareToFirstPassword
-                }
-              ]
-            })(<Input.Password onBlur={this.handleConfirmBlur} />)}
-          </Form.Item>
-          <Form.Item {...tailFormItemLayout}>
-            <Button type="primary" htmlType="button" onClick={this.handleSubmit}>
-              Register
-          </Button>
-          <br/>
-          <a href={FE_LOGIN_URL}> Back to log in </a>
-          </Form.Item>
-          
-        </Form>
-      </div>
+              </Form>
+          </Card>
+        </Layout.Content>
+      </Layout>
     );
   }
 }
